da60b082530981185cfa29a48fddf4bfb1c69c6c
talel <talelmejri8@gmail.com>
2022-11-28 22:54:12 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -2,4 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/js/dist/offcanvas";
+import "bootstrap/js/dist/alert";
+import "bootstrap/js/dist/modal";
+import "bootstrap/js/dist/dropdown";
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook,faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(faSpinner,faFacebook,faLinkedin);
+
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page    <font-awesome-icon  icon="fa-brands fa-linkedin" /></h1>
   </div>
 </template>
