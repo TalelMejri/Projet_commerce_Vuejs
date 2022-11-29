3cbc42ae101ac416fe8483238fa96609e3021799
talel <talelmejri8@gmail.com>
2022-11-29 21:43:29 +0100

    fix bugs

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import "bootstrap/js/dist/offcanvas";
 import "bootstrap/js/dist/alert";
 import "bootstrap/js/dist/modal";
 import "bootstrap/js/dist/dropdown";
+import "./assets/style.css";
+import Vue3Lottie from 'vue3-lottie'
+import 'vue3-lottie/dist/style.css'
+import 'aos/dist/aos.css';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
@@ -15,4 +19,4 @@ import { faFacebook,faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 library.add(faSpinner,faFacebook,faLinkedin);
 
-createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).use(router).use(Vue3Lottie).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
