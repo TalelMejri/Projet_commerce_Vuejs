e255b2d6b3b93a89d94de9525f31b078c8412a04
talel <talelmejri8@gmail.com>
2022-12-02 22:45:11 +0100

    verifier routing for user to enter fo any view

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,60 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import calculer from "../views/CalculaterView.vue";
+import shop from "../views/ShopView.vue";
+import login from "../views/LoginView.vue";
+import signup from "../views/SignUpView.vue";
+import { useAuthStore } from "@/store/auth.store";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
+
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/shop",
+    name: "shop",
+    component: shop,
+    meta: { requiresAuth: true },
+  },
+
+  {
+    path: "/Calculer",
+    name: "Calculer",
+    component: calculer,
+    meta: { requiresAuth: true },
+  },
+
+  {
+    path: "/login",
+    name: "login",
+    component: login,
+  },
+
+  {
+    path: "/Signup",
+    name: "Signup",
+    component: signup,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from) => {
+  const { isAuth } = useAuthStore();
+  if(isAuth&&to.name==="login"){
+    return {name:'home'}
+  }
+  if(to.meta.requiresAuth){
+    if (!isAuth && to.name !== "login") {
+      return { name: "login" };
+    }
+  }
+});
+
+export default router;
