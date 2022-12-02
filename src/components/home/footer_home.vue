e36ee50897d75bd34ac8f973a3a7c8be1844031e
talel <talelmejri8@gmail.com>
2022-12-02 01:47:17 +0100

    add footer

---
+++ src/components/home/footer_home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <footer data-aos="fade-down" class="container-fluid">
+          
+            <hr width="100%"
+               size="5" color="#fff"
+            noshade>
+          <div class="end-footer row text-center p-2">
+              <div class="col-lg-4">
+                © 2023 Bizo - All Rights Reserved
+              </div>
+              <div class="col-lg-4">
+                    Enjoy 😎
+              </div>
+              <div class="col-lg-4">
+                Designed and Developed by Talel Mejri
+            </div>
+          </div>
+       </footer>
+    </div>
+</template>
