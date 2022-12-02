9f05432732ba910a50fa76c93812f7204e9b0644
talel <talelmejri8@gmail.com>
2022-12-02 01:47:52 +0100

    clear default

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,17 @@
 <template>
   <div class="home">
-        <HomeBizoVue></HomeBizoVue>
+        <HomeBizoVue></HomeBizoVue>  
   </div>
 </template>
 
 <script>
-
 import HomeBizoVue from '@/components/HomeBizo.vue';
 
 export default {
   name: 'HomeView',
-  components: {
-
-  },
   components:{
     HomeBizoVue
   },
-
-
-  
 }
 
 </script>
