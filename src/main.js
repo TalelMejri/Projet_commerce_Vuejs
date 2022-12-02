70308b92ff644366f728eb4b3d7acfb8597ead91
talel <talelmejri8@gmail.com>
2022-12-02 01:46:14 +0100

    add icons

--- src/main.js
+++ src/main.js
@@ -8,15 +8,16 @@ import "bootstrap/js/dist/offcanvas";
 import "bootstrap/js/dist/alert";
 import "bootstrap/js/dist/modal";
 import "bootstrap/js/dist/dropdown";
-import "./assets/style.css";
 import Vue3Lottie from 'vue3-lottie'
 import 'vue3-lottie/dist/style.css'
 import 'aos/dist/aos.css';
-
+import "../src/assets/style.css";
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { faFacebook,faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+import { faSpinner,faUser,faComment,faImage,faCertificate,faThumbsUp,faUserGroup, faUserCheck, faCalculator, faShop } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook,faLinkedin,faTwitter,faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-library.add(faSpinner,faFacebook,faLinkedin);
+library.add(faSpinner,faFacebook,faLinkedin,faUser,faTwitter,faYoutube,faComment,faImage,faCertificate,
+    faThumbsUp,faUserGroup,faUserCheck,faCalculator,faShop);
 
 createApp(App).use(router).use(Vue3Lottie).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
