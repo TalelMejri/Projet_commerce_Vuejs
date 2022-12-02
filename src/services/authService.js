f48ccd6e5ad41ada325b7c732a98c685a2710c06
talel <talelmejri8@gmail.com>
2022-12-02 22:45:39 +0100

    auth

---
+++ src/services/authService.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { useAuthStore } from "@/store/auth.store";
+
+export default{
+
+    testLogin(){
+        return axios.get('user');
+    },
+
+    async login(email,password){
+        const State = useAuthStore();
+        const res = await axios.post('auth/login',{email,password})
+        if(res.status==200){
+            State.login(res.data.token,res.data.user);
+        }else{
+           State.logout();
+        }
+    }
+   
+}
