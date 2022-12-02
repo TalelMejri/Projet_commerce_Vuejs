25413ca7ff03507fd38cb6abe301c2c6d61262f9
talel <talelmejri8@gmail.com>
2022-12-02 22:46:56 +0100

    add shop view

---
+++ src/views/ShopView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="mt-5 py-5">
+     <button @click="test">tes</button>
+         <div class="mt-5 py-2">
+              {{store}}
+         </div>
+         {{message}}
+         <button @click="logout">logout</button>
+    </div>
+</template>
+<script>
+import { useAuthStore } from '@/store/auth.store';
+
+export default {
+     setup(){
+          const store=useAuthStore();
+          return {
+               store
+          }
+     },
+     data(){
+          return{
+               message:"talel 9a7ba",
+          }
+     },
+     methods: {
+          test(){
+               console.log(this.store);
+          },
+          logout(){
+               this.store.logout();
+          }
+     },
+}
+</script>
