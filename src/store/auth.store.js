03cf2c343157228c9795862006029aa50490033c
talel <talelmejri8@gmail.com>
2022-12-02 22:46:09 +0100

    add login,logout

---
+++ src/store/auth.store.js
@@ -0,0 +1,37 @@
+import router from "@/router";
+import { defineStore } from "pinia"
+import { computed, ref } from "vue";
+
+export const useAuthStore = defineStore('auth', () => {
+    const token = ref(localStorage.getItem('token')??null);
+    const user = ref(JSON.parse(localStorage.getItem('user'))??null);
+    const isAuth = ref((localStorage.getItem('user')&&localStorage.getItem('token')));
+    
+    const getUser = computed(() => user.value);
+    const getToken = computed(() => token.value);
+    const IsAuth = computed(() => isAuth.value);
+
+    function login(t,u) {
+        token.value = t;
+        user.value = u;
+        isAuth.value = true;
+        localStorage.setItem('token',t);
+        localStorage.setItem('user',JSON.stringify(u));
+    }
+
+    function logout(){
+        isAuth.value = false;
+        token.value = null;
+        user.value = null;
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        router.push({name:"login"});
+
+    }
+    
+
+
+   
+  
+    return { token,user,isAuth,login,logout}
+})
