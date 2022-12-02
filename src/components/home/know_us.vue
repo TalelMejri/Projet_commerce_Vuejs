785a85b624c4a360191589406bc73f4c60ba0bea
talel <talelmejri8@gmail.com>
2022-12-02 01:47:24 +0100

    add knowus

---
+++ src/components/home/know_us.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <article  class="py-2" data-aos="fade-up">
+            <div class="container py-5">
+                    <div class="text-center py-4">
+                        <h2 class="section-heading text-uppercase">U wanna know us</h2>
+                        <h3 class="section-subheading text-muted">Front-End Developer</h3>
+                    </div>
+                    <div class="row ">
+                        <div class="col-lg-12 text-center py-3">
+                                <img class="img_admin" src="../../assets/talelmejri.jpg" alt="..." />
+                                <h4>Talel Mejri</h4>
+                                <p class="text-muted">Fllow Me.</p>
+                                  <a class="button   mx-2" href="#!">
+                                    <font-awesome-icon icon="fa-brands fa-youtube" />
+                                  </a>
+                                  <a class="button   mx-2" href="#!">
+                                    <font-awesome-icon icon="fa-brands fa-twitter" />
+                                </a>
+                                  <a class="button   mx-2" href="#!" >
+                                    <font-awesome-icon icon="fa-brands fa-facebook" />
+                                  </a>
+                                  <a class="button   mx-2" href="#!" >
+                                       <font-awesome-icon icon="fa-brands fa-linkedin" />
+                                  </a>
+                       </div>
+                    </div>
+           </div>
+        </article>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'know',
+    data(){
+        return{
+       
+        }
+    }
+}
+</script>
+
+<style>
+.img_admin{
+    width: 30%;
+    height:50%;
+    border-radius:25px;
+}
+</style>
