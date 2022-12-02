6af046148d16160323575125e511b8545d9f059f
talel <talelmejri8@gmail.com>
2022-12-02 01:46:04 +0100

    fix stylecss

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 <script>
  import BizoLoaderVue from "./components/BizoLoader.vue";
  import NavbarVue from "./components/Navbar.vue";
+
  import AOS from "aos";
   export default{
     data(){
@@ -21,6 +22,7 @@
 
     },
     mounted(){
+    
       AOS.init();
     },
     components:{
@@ -31,42 +33,9 @@
 </script>
 
 <style>
+
 #app{
   background-color: whitesmoke;
 }
-.button{
-  display: inline-block;
-  padding:10px 10px;
-  border:.2rem solid #ff2d75;
-  color:#ff2d75;
-  cursor: pointer;
-  font-size:15px;
-  border-radius: .5rem;
-  position: relative;
-  overflow: hidden;
-  z-index: 0;
-  margin-top: 1rem;
- }
-
-.button::before{
-  content: '';
-  position: absolute;
-  top:0; right: 0;
-  width:0%;
-  height:100%;
-  background:#ff2d75;
-  transition: .3s linear;
-  z-index: -1;
-}
-
-.button:hover::before{
-  width:100%;
-  left: 0;
-}
-
-.button:hover{
-  color:#fff;
-}
-
 
 </style>
