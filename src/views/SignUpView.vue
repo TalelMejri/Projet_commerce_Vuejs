cba2410b9937b19a6b43684d6646c1eb4e9c7a92
talel <talelmejri8@gmail.com>
2022-12-04 23:17:42 +0100

    add messages required for error

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,4 +1,7 @@
 <template>
+  <div>
+    <navbar></navbar>
+
     <div class="mt-5 py-2">
          <div class="mt-5 ">
             <div class="d-flex p-3 m-4 justify-content-center">   
@@ -6,53 +9,50 @@
                       <div class="row shadow-lg rounded">
                             <div class="col-lg-6">
                                     <div class="text-center">
-                                       <h1 class="h4 fw-bolder mb-3 py-3">Sign Up</h1>
-                                    </div>
+                                       <h1 class="h4 fw-bolder mb-3 py-3">Sign Up </h1>
+                                    </div> 
                                 <form class="py-4" @submit.prevent="ADD_user" enctype="multipart/form-data">
                                    <div class="row mb-4 ">
                                       <div class="col-lg-6">
                                        <label>Name</label>
-                                        <input type="text" v-model="name" class="form-control " >
+                                        <input type="text" v-model="name" :class="name_error !='' ?  'form-control is-invalid move_input' : 'form-control' " >
+                                         <small class="text-danger" v-if="name_error">{{name_error}}</small>
                                       </div>
                                       <div class="col-lg-6">
                                         <label>Email</label>
-                                        <input type="email" v-model="email"  class="form-control ">
+                                        <input type="email" v-model="email"  :class="email_error!='' ? 'form-control is-invalid move_input' : 'form-control' ">
+                                        <small class="text-danger" v-if="email_error">{{email_error}}</small>
                                       </div>
                                     </div>
                                     <div class="row mb-4">
                                       <div class="col-lg-12">
                                       <label>Password </label>
                                     <div class="input-group">
-                                    <input type="password" v-model="password" id="pass" class="form-control" >
+                                    <input :type="type_password" v-model="password"  :class="password_error!='' ? 'form-control is-invalid move_input' : 'form-control'" >
                                      <span class="input-group-btn">
-                                       <button id="changer_visible" class="btn btn-primary" type="button">
-                                       <svg width="24" height="24" fill="none" viewBox="0 0 24 24" class="eye hidden">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M19.25 12C19.25 13 17.5 18.25 12 18.25C6.5 18.25 4.75 13 4.75 12C4.75 11 6.5 5.75 12 5.75C17.5 5.75 19.25 11 19.25 12Z"></path>
-                                            <circle cx="12" cy="12" r="2.25" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></circle>
-                                        </svg>
-                                        <svg width="24" height="24" fill="none" viewBox="0 0 24 24" class="eyeOff">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M18.6247 10C19.0646 10.8986 19.25 11.6745 19.25 12C19.25 13 17.5 18.25 12 18.25C11.2686 18.25 10.6035 18.1572 10 17.9938M7 16.2686C5.36209 14.6693 4.75 12.5914 4.75 12C4.75 11 6.5 5.75 12 5.75C13.7947 5.75 15.1901 6.30902 16.2558 7.09698"></path>
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M19.25 4.75L4.75 19.25"></path>
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M10.409 13.591C9.53033 12.7123 9.53033 11.2877 10.409 10.409C11.2877 9.5303 12.7123 9.5303 13.591 10.409"></path>
-                                        </svg> 
-                                     </button>
+                                       <button id="changer_visible" @click="changer_visib_password()" class="btn btn-primary" type="button">
+                                          <font-awesome-icon :icon="`fa-solid fa-${visibilite_password}`"/>
+                                      </button>
                                     </span>
+                               
                                   </div>  
-                                
+                                  <small class="text-danger" v-if="password_error">{{password_error}}</small>
                                 </div>
                                   </div>
                                     <div class="row">
                                       <div class="col-lg-12 mb-2">
                                         <label>Date Naissance :</label>
-                                        <input type="date" v-model="date_naissance"  class="form-control ">
+                                        <input type="date" v-model="date_naissance"  :class="date_naissance_error !='' ? 'form-control is-invalid move_input': 'form-control' ">
+                                        <small class="text-danger" v-if="date_naissance_error">{{date_naissance_error}}</small>
                                       </div>
                                       <div class="col-lg-12">
                                         <label>Avatar</label>
-                                        <input type="file" ref="file"  @change="upload_image" class="form-control ">
+                                        <input type="file" ref="file"  @change="upload_image" :class="file_error !='' ? 'form-control is-invalid move_input' : 'form-control' ">
+                                        <small class="text-danger" v-if="file_error">{{file_error}}</small>
                                       </div>
                                     </div>
                                        <div class="form-group text-center py-4">
-                                              <input type="checkbox"  id="agree" class="custom-control-input">
+                                              <input type="checkbox" required  id="agree" class="custom-control-input">
                                               <label for="agree" class="custom-control-label">I agree to the <a href="#">Terms and Conditions</a></label>
                                     </div>
                                     <div class="text-center mt-2">
@@ -76,13 +76,14 @@
         </div>
          </div>
     </div>
+  </div>
 </template>
 
 <script>
 //import axios from 'axios';
 
 import service from '../services/service';
-
+import navbar from "@/components/Navbar.vue";
 export default{
   name:'signUpView',
 
@@ -93,12 +94,22 @@ export default{
       email:"",
       file:"",
       date_naissance:"",
-      //data:{}
+      
+      email_error:"",
+      name_error:"",
+      date_naissance_error:"",
+      password_error:"",
+      file_error:"",
+      visibilite_password:'eye',
+      type_password:'password'
     }
   },
   
-  methods:{
-
+  methods:{ 
+    changer_visib_password(){
+      this.visibilite_password= this.visibilite_password=='eye-slash' ? 'eye' : 'eye-slash';  
+      this.type_password=this.visibilite_password=='eye' ? 'text' : 'password';
+    },
    upload_image() {
        this.file = this.$refs.file.files[0];
     },
@@ -117,11 +128,37 @@ export default{
             this.date_naissance = "" ;
             this.$router.push({name:"login", query: {content: 'Register Succesfully'}});
         }).catch((error)=>{
-            console.log(error.response.data);
+            this.name_error=error.response.data.data.nom ? error.response.data.data.nom[0]: '';
+            this.email_error=error.response.data.data.email ? error.response.data.data.email[0]: '';
+            this.date_naissance_error=error.response.data.data.date_naissance  ? error.response.data.data.date_naissance [0] : '';
+            this.password_error=error.response.data.data.password ? error.response.data.data.password  [0] : '';
+            this.file_error=error.response.data.data.file ? error.response.data.data.file [0] : '';
         }) ;
     }
     
+  },
+  components:{
+     navbar
   }
 }
 
 </script>
+
+
+<style>
+.move_input {
+  animation: animate 0.5s;
+}
+
+@keyframes animate {
+  0% {
+    transform: translateX(10px);
+  }
+  50% {
+    transform: translateX(-10px);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+</style>
