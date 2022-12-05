4e2b6c1e85f116197529bae26d8cd8dcf7272913
talel <talelmejri8@gmail.com>
2022-12-05 22:51:54 +0100

    add checkadmin

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,8 @@ import calculer from "../views/CalculaterView.vue";
 import shop from "../views/ShopView.vue";
 import login from "../views/LoginView.vue";
 import signup from "../views/SignUpView.vue";
-import { useAuthStore } from "@/store/auth.store";
+import ProfilAdmin from "../views/ProfilAdminView.vue";
+//import { useAuthStore } from "@/store/auth.store";
 
 const routes = [
   {
@@ -17,14 +18,21 @@ const routes = [
     path: "/shop",
     name: "shop",
     component: shop,
-    meta: { requiresAuth: true },
+    //meta: { requiresAuth: true },
+  },
+
+  {
+    path: "/ProfilAdmin",
+    name: "ProfilAdmin",
+    component: ProfilAdmin,
+    //meta: { requiresAuth: true },
   },
 
   {
     path: "/Calculer",
     name: "Calculer",
     component: calculer,
-    meta: { requiresAuth: true },
+   // meta: { requiresAuth: true },
   },
 
   {
@@ -38,26 +46,34 @@ const routes = [
     name: "Signup",
     component: signup,
   },
+
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
-
+/*
 router.beforeEach((to, from) => {
   
-  const { isAuth } = useAuthStore();
-  if(isAuth&&to.name==="login"){
+  const isAuth = useAuthStore();
+/*
+  if(isAuth.isAuth &&to.name==="login"){
     return {name:'home'}
   }
 
   if(to.meta.requiresAuth){
-    if (!isAuth && to.name !== "login") {
+    if (!isAuth.isAuth && to.name !== "login") {
       return { name: "login" };
     }
   }
 
-});
+  if(to.meta.requiresAuth){
+    if (!isAuth.isAuth && !isAuth.isAdmin &&  to.name !== "login") {
+      return { name: "login" };
+    }
+  }
+*/
+//});
 
 export default router;
--- src/store/auth.store.js
+++ src/store/auth.store.js
@@ -7,29 +7,40 @@ export const useAuthStore = defineStore('auth', () => {
     const token = ref(localStorage.getItem('token')??null);
     const user = ref(JSON.parse(localStorage.getItem('user'))??null);
     const isAuth = ref((localStorage.getItem('user')&&localStorage.getItem('token')));
+    const isAdmin = ref(localStorage.getItem('Isadmin')??null);
     
     const getUser = computed(() => user.value);
     const getToken = computed(() => token.value);
     const IsAuth = computed(() => isAuth.value);
+    const getadmin = computed(() => isAdmin.value);
 
-    function login(t,u) {
+    function login(t,u,check) {
         token.value = t;
         user.value = u;
+        isAdmin.value=check;
         isAuth.value = true;
         localStorage.setItem('token',t);
+        localStorage.setItem('Isadmin',check);
         localStorage.setItem('user',JSON.stringify(u));
-        router.push({name:"shop"});
+        if(check==true){
+            router.push({name:"ProfilAdmin"});
+        }else{
+            router.push({name:"shop"});
+        }
+     
     }
 
     function logout(){
         isAuth.value = false;
         token.value = null;
         user.value = null;
+        isAdmin.value=null;
         localStorage.removeItem('token');
         localStorage.removeItem('user');
+        localStorage.removeItem('Isadmin');
         router.push({name:"login"});
     }
 
-    return { token,user,isAuth,login,logout}
+    return { token,user,isAuth,isAdmin,login,logout,getUser}
 
 })
