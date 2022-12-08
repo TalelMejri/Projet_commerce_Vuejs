26cadb80c5a5964d9dbc400d91737db058b2da4a
talel <talelmejri8@gmail.com>
2022-12-08 03:53:55 +0100

    add confetti,jspdf

--- src/main.js
+++ src/main.js
@@ -20,6 +20,8 @@ import { faFacebook,faLinkedin,faTwitter,faYoutube } from '@fortawesome/free-bra
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { createPinia } from 'pinia';
 import { jwtInterceptor } from './globals/interceptors';
+import Vue from 'vue';
+import VueConfetti from 'vue-confetti';
 
 library.add(faSpinner,faFacebook,faLinkedin,faUser,faTwitter,faAdd,faYoutube,faComment,faCircleCheck,faImage,faCertificate,faBars,faCartShopping,
     faThumbsUp,faUserGroup,faList,faUserCheck,faCalculator,faShop,faEyeSlash,faEye,faBell,faAngleRight,faCircle,faHouseUser);
@@ -28,4 +30,4 @@ const pinia = createPinia()
 
 jwtInterceptor()
 
-createApp(App).use(pinia).use(HighchartsVue).use(router).use(Vue3Lottie).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).use(pinia).use(VueConfetti).use(HighchartsVue).use(router).use(Vue3Lottie).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
