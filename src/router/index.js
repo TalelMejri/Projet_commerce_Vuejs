9c2d95a319ef756b535b21bb9ce26418569a1dba
talel <talelmejri8@gmail.com>
2022-12-11 22:03:14 +0100

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import login from "../views/LoginView.vue";
 import signup from "../views/SignUpView.vue";
 import ProfilAdmin from "../views/ProfilAdminView.vue";
 import editProduct from "../views/Product/EditProductView.vue";
-//import { useAuthStore } from "@/store/auth.store";
+import { useAuthStore } from "@/store/auth.store";
 
 const routes = [
   {
@@ -19,27 +19,28 @@ const routes = [
     path: "/editProduct/:id",
     name: "editProduct",
     component: editProduct,
+    meta: { requiresAuth: true,requiresAdmin:true },
   },
 
   {
     path: "/shop",
     name: "shop",
     component: shop,
-    //meta: { requiresAuth: true },
+    meta: { requiresAuth: true, requiresUser:true },
   },
 
   {
     path: "/ProfilAdmin",
     name: "ProfilAdmin",
     component: ProfilAdmin,
-    //meta: { requiresAuth: true },
+    meta: { requiresAuth: true, requiresAdmin:true },
   },
 
   {
     path: "/Calculer",
     name: "Calculer",
     component: calculer,
-   // meta: { requiresAuth: true },
+   meta: { requiresAuth: true, requiresUser:true },
   },
 
   {
@@ -62,27 +63,23 @@ const router = createRouter({
 
 });
 
-/*
 router.beforeEach((to, from) => {
-  
   const isAuth = useAuthStore();
-/*
+  let isAdmin = JSON.parse(isAuth.getIsAdmin());
+  let isClient = JSON.parse(isAuth.getIsClient());
   if(isAuth.isAuth &&to.name==="login"){
     return {name:'home'}
   }
-
   if(to.meta.requiresAuth){
     if (!isAuth.isAuth && to.name !== "login") {
       return { name: "login" };
-    }
-  }
-
-  if(to.meta.requiresAuth){
-    if (!isAuth.isAuth && !isAuth.isAdmin &&  to.name !== "login") {
+    }else if(to.meta.requiresAdmin && !isAdmin){
+      return { name: "login" };
+    }else if(to.meta.requiresUser && !isClient){
       return { name: "login" };
     }
   }
-*/
-//});
+
+});
 
 export default router;
