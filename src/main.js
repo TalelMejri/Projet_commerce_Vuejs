e22a18acb97032a5378772c9eb7c3aa215c401ac
talel <talelmejri8@gmail.com>
2022-12-11 22:01:40 +0100

    fix import

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ import 'aos/dist/aos.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 
 import HighchartsVue from 'highcharts-vue'
-import { faSpinner,faUser,faComment,faImage,faCertificate,faAdd,faList,faBell,faCircleCheck,faThumbsUp,faUserGroup,faBars,faCartShopping, faUserCheck, faCalculator, faShop,faEyeSlash,faEye,faHouseUser,faAngleRight,faCircle} from '@fortawesome/free-solid-svg-icons';
+import { faSpinner,faUser,faComment,faImage,faCertificate,faAdd,faList,faBell,faCircleCheck,faThumbsUp,faUserGroup,faBars,faCartShopping, faUserCheck, faCalculator, faShop,faEyeSlash,faEye,faHouseUser,faAngleRight,faCircle,faGrip} from '@fortawesome/free-solid-svg-icons';
 import { faFacebook,faLinkedin,faTwitter,faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { createPinia } from 'pinia';
@@ -24,7 +24,7 @@ import Vue from 'vue';
 import VueConfetti from 'vue-confetti';
 
 library.add(faSpinner,faFacebook,faLinkedin,faUser,faTwitter,faAdd,faYoutube,faComment,faCircleCheck,faImage,faCertificate,faBars,faCartShopping,
-    faThumbsUp,faUserGroup,faList,faUserCheck,faCalculator,faShop,faEyeSlash,faEye,faBell,faAngleRight,faCircle,faHouseUser);
+    faThumbsUp,faUserGroup,faList,faUserCheck,faCalculator,faShop,faEyeSlash,faEye,faBell,faAngleRight,faCircle,faHouseUser,faGrip);
 
 const pinia = createPinia()
 
